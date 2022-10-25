8407384b83264bd5a50c9761cda77bb52d8386a6
SOunit <workbookwork7@gmail.com>
2022-10-25 07:50:52 -0700

    add sample

---
+++ AspNetIdentitySample/Controllers/AccountController.cs
@@ -0,0 +1,62 @@
+﻿using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using AspNetIdentitySample.Identity;
+using AspNetIdentitySample.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+
+namespace AspNetIdentitySample.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account/Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: Account/Register
+        [HttpPost]
+        public ActionResult Register(RegisterViewModel rvm)
+        {
+            if (ModelState.IsValid)
+            {
+                // register
+                var appDbContext = new ApplicationDbContext();
+                var userStore = new ApplicationUserStore(appDbContext);
+                var userManager = new ApplicationUserManager(userStore);
+                var passwordHash = Crypto.HashPassword(rvm.Password);
+                var user = new ApplicationUser()
+                {
+                    Email = rvm.Email,
+                    UserName = rvm.Username,
+                    PasswordHash = passwordHash,
+                    City = rvm.City,
+                    Country = rvm.Country,
+                    Birthday = rvm.DateOfBirth,
+                    Address = rvm.Address,
+                    PhoneNumber = rvm.Mobile
+                };
+                IdentityResult result = userManager.Create(user);
+                if (result.Succeeded)
+                {
+                    // role
+                    userManager.AddToRole(user.Id, "Customer");
+
+                    // login
+                    var authenticationManager = HttpContext.GetOwinContext().Authentication;
+                    var userIdentity = userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+                    authenticationManager.SignIn(new AuthenticationProperties(), userIdentity);
+                }
+
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("My Error", "Invalid Data");
+                return View();
+            }
+        }
+    }
+}
---
+++ AspNetIdentitySample/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNetIdentitySample.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Username can't be blank")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password can't be blank")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password can't be blank")]
+        [Compare("Password", ErrorMessage = "Password and confirm password do not match")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Email can't be blank")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string Mobile { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+    }
+}
